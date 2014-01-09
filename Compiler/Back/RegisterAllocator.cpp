b74efa742a6e1647d2fa4cccd97009ebb9da744d
Matt Fischer <mattfischer84@gmail.com>
2014-01-08 23:48:04 -0600

    Added preferred registers to register allocator

--- Compiler/Back/RegisterAllocator.cpp
+++ Compiler/Back/RegisterAllocator.cpp
@@ -16,9 +16,21 @@ namespace Back {
 static const int MaxRegisters = 13;
 static const int CallerSavedRegisters = 4;
 
+void addInterferences(Analysis::InterferenceGraph &graph, const Analysis::InterferenceGraph::SymbolSet &symbols, IR::Symbol *symbol, IR::Symbol *exclude)
+{
+	for(Analysis::LiveVariables::SymbolSet::const_iterator symbolIt = symbols.begin(); symbolIt != symbols.end(); symbolIt++) {
+		IR::Symbol *otherSymbol = *symbolIt;
+		if(otherSymbol != exclude) {
+			graph.addEdge(symbol, otherSymbol);
+		}
+	}
+}
+
 std::map<IR::Symbol*, int> RegisterAllocator::allocate(IR::Procedure *procedure)
 {
 	std::map<IR::Symbol*, int> registers;
+	std::map<IR::Symbol*, int> preferredRegisters;
+
 	Analysis::InterferenceGraph graph(procedure);
 	Analysis::LiveVariables liveVariables(procedure);
 
@@ -34,43 +46,57 @@ std::map<IR::Symbol*, int> RegisterAllocator::allocate(IR::Procedure *procedure)
 
 	for(IR::EntryList::iterator entryIt = procedure->entries().begin(); entryIt != procedure->entries().end(); entryIt++) {
 		IR::Entry *entry = *entryIt;
+		IR::EntryThreeAddr *threeAddr = (IR::EntryThreeAddr*)entry;
+		IR::EntryTwoAddrImm *twoAddrImm = (IR::EntryTwoAddrImm*)entry;
+
 		const Analysis::LiveVariables::SymbolSet &variables = liveVariables.variables(entry);
-		for(Analysis::LiveVariables::SymbolSet::const_iterator symbolIt = variables.begin(); symbolIt != variables.end(); symbolIt++) {
-			IR::Symbol *symbol = *symbolIt;
-			IR::EntryThreeAddr *threeAddr = (IR::EntryThreeAddr*)entry;
-			IR::EntryTwoAddrImm *twoAddrImm = (IR::EntryTwoAddrImm*)entry;
 
-			switch(entry->type) {
-				case IR::Entry::TypeCall:
-					for(int i=0; i<CallerSavedRegisters; i++) {
-						graph.addEdge(symbol, callerSavedRegisters[i]);
-					}
-					break;
+		switch(entry->type) {
+			case IR::Entry::TypeCall:
+				for(int i=0; i<CallerSavedRegisters; i++) {
+					addInterferences(graph, variables, callerSavedRegisters[i], 0);
+				}
+				break;
 
-				case IR::Entry::TypeLoadRet:
-					if(symbol != threeAddr->lhs) {
-						graph.addEdge(symbol, callerSavedRegisters[0]);
-					}
-					break;
+			case IR::Entry::TypeLoadRet:
+				if(preferredRegisters.find(threeAddr->lhs) == preferredRegisters.end()) {
+					preferredRegisters[threeAddr->lhs] = 0;
+				} else {
+					preferredRegisters[threeAddr->lhs] = -1;
+				}
 
-				case IR::Entry::TypeStoreRet:
-					if(symbol != threeAddr->rhs1) {
-						graph.addEdge(symbol, callerSavedRegisters[0]);
-					}
-					break;
+				addInterferences(graph, variables, callerSavedRegisters[0], threeAddr->lhs);
+				break;
 
-				case IR::Entry::TypeLoadArg:
-					if(symbol != twoAddrImm->lhs) {
-						graph.addEdge(symbol, callerSavedRegisters[twoAddrImm->imm]);
-					}
-					break;
+			case IR::Entry::TypeStoreRet:
+				if(preferredRegisters.find(threeAddr->rhs1) == preferredRegisters.end()) {
+					preferredRegisters[threeAddr->rhs1] = 0;
+				} else {
+					preferredRegisters[threeAddr->rhs1] = -1;
+				}
 
-				case IR::Entry::TypeStoreArg:
-					if(symbol != twoAddrImm->rhs) {
-						graph.addEdge(symbol, callerSavedRegisters[twoAddrImm->imm]);
-					}
-					break;
-			}
+				addInterferences(graph, variables, callerSavedRegisters[0], threeAddr->rhs1);
+				break;
+
+			case IR::Entry::TypeLoadArg:
+				if(preferredRegisters.find(twoAddrImm->lhs) == preferredRegisters.end()) {
+					preferredRegisters[twoAddrImm->lhs] = twoAddrImm->imm;
+				} else {
+					preferredRegisters[twoAddrImm->lhs] = -1;
+				}
+
+				addInterferences(graph, variables, callerSavedRegisters[twoAddrImm->imm], twoAddrImm->lhs);
+				break;
+
+			case IR::Entry::TypeStoreArg:
+				if(preferredRegisters.find(twoAddrImm->rhs) == preferredRegisters.end()) {
+					preferredRegisters[twoAddrImm->rhs] = twoAddrImm->imm;
+				} else {
+					preferredRegisters[twoAddrImm->rhs] = -1;
+				}
+
+				addInterferences(graph, variables, callerSavedRegisters[twoAddrImm->imm], twoAddrImm->rhs);
+				break;
 		}
 	}
 
@@ -104,19 +130,30 @@ std::map<IR::Symbol*, int> RegisterAllocator::allocate(IR::Procedure *procedure)
 
 		const Analysis::InterferenceGraph::SymbolSet &set = graph.interferences(symbol);
 
-		for(int i=0; i<MaxRegisters; i++) {
+		for(int i=-1; i<MaxRegisters; i++) {
 			bool found = false;
+
+			int reg;
+			if(i == -1) {
+				if(preferredRegisters.find(symbol) != preferredRegisters.end() && preferredRegisters[symbol] != -1) {
+					reg = preferredRegisters[symbol];
+				} else {
+					continue;
+				}
+			} else {
+				reg = i;
+			}
 
 			for(Analysis::InterferenceGraph::SymbolSet::const_iterator symbolIt = set.begin(); symbolIt != set.end(); symbolIt++) {
 				IR::Symbol *otherSymbol = *symbolIt;
-				if(registers.find(otherSymbol) != registers.end() && registers.find(otherSymbol)->second == i) {
+				if(registers.find(otherSymbol) != registers.end() && registers.find(otherSymbol)->second == reg) {
 					found = true;
 					break;
 				}
 			}
 
 			if(!found) {
-				registers[symbol] = i;
+				registers[symbol] = reg;
 				break;
 			}
 		}
