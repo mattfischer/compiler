5234158ae9616506dfb6a8fc79d08b25e1c60b68
Matt Fischer <mattfischer84@gmail.com>
2014-03-29 00:26:18 -0500

    Added preliminary allocate/free algorithm to Heap

--- Compiler/VM/Heap.cpp
+++ Compiler/VM/Heap.cpp
@@ -2,6 +2,10 @@
 
 namespace VM {
 
+	const int WordSize = sizeof(unsigned long);
+	const int MinSize = 2 * WordSize;
+	const int PrevInUseBit = 0x1;
+
 	Heap::Heap(unsigned char *mem, int memSize, int start)
 	{
 		mMem = mem;
@@ -9,13 +13,94 @@ namespace VM {
 		mStart = start;
 
 		mUsedSize = 0;
+		mFreeList = 0;
 	}
 
-	int Heap::allocate(int size)
+	unsigned int Heap::allocate(unsigned int size)
 	{
-		int index = mStart + mUsedSize;
-		mUsedSize += size;
+		if(size % WordSize != 0) {
+			size += WordSize - size % WordSize;
+		}
 
-		return index;
+		if(size < MinSize) {
+			size = MinSize;
+		}
+
+		size += WordSize;
+
+		Header *header = 0;
+		Header *cursor = mFreeList;
+		while(cursor) {
+			if(cursor->size >= size) {
+				header = cursor;
+				removeFree(header);
+
+				Header *nextHeader = getHeader(getIndex(header) + header->size);
+				nextHeader->size |= PrevInUseBit;
+				break;
+			}
+			cursor = cursor->nextFree;
+		}
+
+		if(!header) {
+			int index = mStart + mUsedSize + WordSize;
+			mUsedSize += size;
+
+			header = getHeader(index);
+			header->size = size;
+			header->size |= PrevInUseBit;
+		}
+
+		return getIndex(header);
+	}
+
+	void Heap::free(unsigned int index)
+	{
+		Header *header = getHeader(index);
+		Header *nextHeader = getHeader(index + header->size);
+
+		nextHeader->size &= ~PrevInUseBit;
+		nextHeader->prevSize = header->size;
+
+		if(index == mStart + mUsedSize - header->size) {
+			mUsedSize -= header->size;
+		} else {
+			pushFree(header);
+		}
+	}
+
+	Heap::Header *Heap::getHeader(unsigned int index)
+	{
+		return (Header*)(mMem + index - 2 * WordSize);
+	}
+
+	unsigned long Heap::getIndex(Header *header)
+	{
+		return (unsigned long)((unsigned char*)header - (unsigned char*)mMem) + 2 * WordSize;
+	}
+
+	void Heap::pushFree(Header *header)
+	{
+		if(mFreeList) {
+			mFreeList->prevFree = header;
+		}
+		header->nextFree = mFreeList;
+		header->prevFree = 0;
+		mFreeList = header;
+	}
+
+	void Heap::removeFree(Header *header)
+	{
+		if(header->prevFree) {
+			header->prevFree->nextFree = header->nextFree;
+		}
+
+		if(header->nextFree) {
+			header->nextFree->prevFree = header->prevFree;
+		}
+
+		if(header == mFreeList) {
+			mFreeList = header->nextFree;
+		}
 	}
 }
--- Compiler/VM/Heap.h
+++ Compiler/VM/Heap.h
@@ -6,14 +6,28 @@ namespace VM {
 	public:
 		Heap(unsigned char *mem, int memSize, int start);
 
-		int allocate(int size);
+		unsigned int allocate(unsigned int size);
+		void free(unsigned int index);
 
 	private:
+		struct Header {
+			unsigned long prevSize;
+			unsigned long size;
+			Header *prevFree;
+			Header *nextFree;
+		};
+
+		Header *getHeader(unsigned int index);
+		unsigned long getIndex(Header *header);
+		void pushFree(Header *header);
+		void removeFree(Header *header);
+
 		unsigned char *mMem;
 		int mMemSize;
 		int mStart;
 
 		int mUsedSize;
+		Header *mFreeList;
 	};
 }
 
