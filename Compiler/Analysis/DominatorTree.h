ee25a5de731d44bc06ae83af864dbc8fb35c1aed
Matt Fischer <mattfischer84@gmail.com>
2017-10-22 00:16:29 -0500

    Remove prototypes for non-existant functions

--- Compiler/Analysis/DominatorTree.h
+++ Compiler/Analysis/DominatorTree.h
@@ -24,9 +24,6 @@ namespace Analysis {
 		bool dominates(const FlowGraph::Block *block, const FlowGraph::Block *dominator) const;
 
 	private:
-		void topoSortRecurse(const FlowGraph::Block *block, std::vector<const FlowGraph::Block*> &blocks, std::set<const FlowGraph::Block*> &seenBlocks);
-		std::vector<const FlowGraph::Block*> topologicalSort(std::vector<FlowGraph::Block*> &blocks);
-
 		std::vector<const FlowGraph::Block*> mBlocks; //!< List of blocks
 		std::map<const FlowGraph::Block*, const FlowGraph::Block*> mIDoms; //!< List of immediate dominators
 	};
